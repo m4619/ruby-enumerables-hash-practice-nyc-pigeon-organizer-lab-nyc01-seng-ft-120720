60ee9a9da23f32a985051c2c8bdee178e5a3b401
Miguel  Arreola <marreola4619@gmail.com>
2020-11-18 20:16:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,20 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
-  binding.pry
+  data.each_with_object({}) do |(key, value), final_array|
+  # binding.pry
+  value.each do |inner_key, names|
+    name.each do |name|
+      #binding.pry
+      if !final_array[name]
+        final_array[name] = {}
+      end
+      if !final_array[name][key]
+        !final_array[name][key] = []
+      end
+      final_array[name][key].push(inner_key.to_s)
+    end
+  end
+  end
+  #binding.pry  
 end
